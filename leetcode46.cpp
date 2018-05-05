f6dd3dbf034ca4ca8d5e6f0768e19c1b9267bdd9
ParticularJ <344409038@qq.com>
2018-05-05 23:40:51 +0800

    leet

--- leetcode46.cpp
+++ leetcode46.cpp
@@ -3,20 +3,29 @@
 
 using namespace std;
 
-//
+//使用回溯法
 
 class Solution{
+public:
+  void helper(vector<vector<int>>& res, vector<int>& temp, vector<int>& nums){
+    if(temp.size()==nums.size()){
+      res.push_back(temp);
+      return;
+    }
+    for(int i=0;i<nums.size();i++){
+      if(temp[nums[i]])continue;
+      temp.push_back(nums[i]);
+      helper(res, temp, nums);
+      temp.erase(temp.end()-1);
+    }
+  }
 public:
   vector<vector<int>> permute(vector<int>& nums){
     vector<vector<int>> res;
     if(!nums.size())return res;
-    for(int i=0;i<nums.size();i++){
-      vector<int> temp;
-      temp.push_back(nums[i]);
-      for(int j=i+1;j<nums.size();j++){
-        if()
-      }
-    }
+    vector<int> temp;
+    helper(res, temp, nums);
+    return res;
   }
 };
 
