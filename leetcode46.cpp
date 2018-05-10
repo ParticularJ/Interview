b1c1fa05ca3387b7b440d9e24e44ef9a9803661b
ParticularJ <344409038@qq.com>
2018-05-10 23:44:02 +0800

    -mleet

--- leetcode46.cpp
+++ leetcode46.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-//使用回溯法
+//按顺序添加之后，交换位置，再次添加，直到结束。
 
 class Solution{
 public:
@@ -16,9 +16,11 @@ public:
       res.push_back(nums);
       if(size==0)return res;
       int assend = size-1;
+      // 通过assend判断是否降序完成，
       while(assend!=0 && nums[assend-1]>nums[assend])
         assend -= 1;
       if(assend==0)break;
+      // 与前一个比较，如果后一个大于前一个交换二者位置
       int mini = nums[assend-1];
       for(int i=size-1;i>=assend;i--){
         if(nums[i]>mini){
@@ -26,6 +28,7 @@ public:
           break;
         }
       }
+      // 类似排序，如将上一步得到的2，3，1，变为2，1，3
       for(int i=assend, j=size-1;i <= j; i++,j--){
         swap(nums[i], nums[j]);
       }
@@ -36,7 +39,7 @@ public:
 
 int main(){
   Solution p;
-  vector<int> a = {1, 2, 3};
+  vector<int> a = {1, 2, 3, 4};
   vector<vector<int>> b;
   b = p.permute(a);
   for(auto c:b){
