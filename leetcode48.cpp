0d2a610d3cd25b07715125a63725bebb486e5ff0
ParticularJ <344409038@qq.com>
2018-05-15 23:19:35 +0800

    leet

--- leetcode48.cpp
+++ leetcode48.cpp
@@ -5,27 +5,37 @@ using namespace std;
 
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
+    vector<vector<int>> rotate(vector<vector<int>>& matrix) {
+        vector<vector<int>> res;
+        res = matrix;
         int row = matrix.size();
         for(int i=0;i<row;i++){
             for(int j=row-1;j>=i;j--){
-                matrix[i][j]=matrix[j][i];
-                cout<<count;
+                int temp = res[i][j];
+                res[i][j] = res[j][i];
+                res[j][i] = temp;
             }
         }
-        for(int i=0;i<row;i++){
-          for(int j=0;j<col;)
-          matrix[i][i]
-        }
-    }
+      //  return res;
+        int count=row-1;
+         for(int i=0;i<int(row/2);i++){
+           for(int j=0;j<row;j++){
+             int temp=res[j][i];
+             res[j][i]=res[j][count];
+             res[j][count]=temp;
+           }
+           count--;
+         }
+    return res;
+  }
 };
 
 
 int main(){
   vector<vector<int>> a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  vector<vector<int>> b;
   Solution rot;
-  b = rot.rotate(a);
+  vector<vector<int>> b;
+  b=rot.rotate(a);
   for(auto c:b){
     for(auto d:c){
       cout<<d;
