ca599ad5fe43ce26d360b8a94d6cb68d3bc2a8a8
ParticularJ <344409038@qq.com>
2018-05-19 23:55:23 +0800

    leet

---
+++ leetcode300.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+  int lengthOfLIS(vector<int>& nums){
+
+  }
+};
+
+int main(){
+  Solution lOLIS;
+  vector<int> a = {10, 9, 2, 5, 3, 7, 101, 18};
+  cout<<lOLIS.lengthOfLIS(a);
+  return 0;
+}
