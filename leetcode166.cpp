cdc3cff899d70e4a9b0e894db11f9322e26bcf79
ParticularJ <344409038@qq.com>
2018-05-24 23:52:20 +0800

    leet

---
+++ leetcode166.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+class Solution{
+public:
+  string fractionToDecimal(int numerator, int denominator){
+    
+  }
+};
+
+
+int main(){
+  int a,b;
+  cin>>a,b;
+  Soluton fTD;
+  fTD.fractionToDecimal(a,b);
+  return 0;
+}
