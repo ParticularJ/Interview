d605a3286afb59413e389d0007359cb538584731
ParticularJ <344409038@qq.com>
2018-05-25 22:38:13 +0800

    leet

--- leetcode166.cpp
+++ leetcode166.cpp
@@ -5,6 +5,19 @@
 class Solution{
 public:
   string fractionToDecimal(int numerator, int denominator){
+    if(numerator==0)return "0";
+    string res="";
+    long long num = numerator;
+    long long den = denominator;
+    int sign = 1;
+    if(num<0){
+      sign = -sign;
+      num=-num;
+    }
+    if(den<0){
+      sign = -sign;
+      den = -den;
+    }
     
   }
 };
