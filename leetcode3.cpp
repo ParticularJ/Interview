2685d9f0e5a14743a0eebf8aef23f8fa85bed76d
ParticularJ <344409038@qq.com>
2018-06-10 23:26:12 +0800

    leet

--- leetcode3.cpp
+++ leetcode3.cpp
@@ -8,21 +8,7 @@ using namespace std;
 
 class Solution{
 public:
-  // int lengthOfLongestSubstring(string s){
-  //   int rea=0;
-  //   int size = s.size();
-  //   if(!size)return res;
-  //   unordered_map<int, int> mymap;
-  //   int begin=0,end = 0;
-  //   for(int i=0;i<size;i++){
-  //     mymap[s[i]-'a']++;
-  //   }
-  //   for(int i=0;i<size;i++){
-  //     if(mymap[s[i]-'a'])
-  //   }
-  // }
-public:
-  int lengthOfLongestSubstring(string s){
+  int lengthOfLongestSubstring2(string s){
     vector<int> dict(256, -1);
     int size=s.size();
     int maxlen=0,start=-1;
@@ -35,3 +21,10 @@ public:
     return maxlen;
   }
 };
+
+int main(){
+  string a = "abcabcbb";
+  Solution lOLS;
+  cout<<lOLS.lengthOfLongestSubstring1(a);
+  return 0;
+}
