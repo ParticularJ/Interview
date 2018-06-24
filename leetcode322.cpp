92067a46d985af9215192c90f39e1dd0bdc64f92
ParticularJ <344409038@qq.com>
2018-06-24 23:31:20 +0800

    leet

--- leetcode322.cpp
+++ leetcode322.cpp
@@ -6,13 +6,12 @@ using namespace std;
 class Solution{
 public:
 	int coinChange(vector<int>& coins, int amount) {
-		int size = coins.size();
-		vector<int> f(size + 1, -1);
+		vector<int> f(amount + 1, -1);
 		f[0] = 0;
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j + coins[i] <= amount; j++) {
 				if (f[j] != -1) {
-					f[j + coins[i]] = min(f[j + coins[i]] == -1 ? (1 << 30) : f[j + coins[i], f[j] + 1);
+					f[j + coins[i]] = min(f[j + coins[i]] == -1 ? (1 << 30) : f[j + coins[i]], f[j] + 1);
 				}
 			}
 		}
