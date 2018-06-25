f8571477e91ace3fa3c752df37b71e6f376d26c6
ParticularJ <344409038@qq.com>
2018-06-25 23:52:39 +0800

    leet

--- leetcode322.cpp
+++ leetcode322.cpp
@@ -3,6 +3,7 @@
 #include<algorithm>
 using namespace std;
 // 完全背包问题，并且个数不限，我们只要能填满即可
+// f[j]+1 判断背包现在的情况，将当前的重量放入背包，比较价值
 class Solution{
 public:
 	int coinChange(vector<int>& coins, int amount) {
