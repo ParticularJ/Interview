59790588c4b9f17f695694a484edd8feba87248f
ParticularJ <344409038@qq.com>
2018-07-04 23:35:54 +0800

    llet

---
+++ leetcode134.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+		int size = gas.size();
+		int a = 0;
+		vector<int> temp;
+		for (int i = 0; i < size; i++) {
+			a += gas[i] - cost[i];
+			temp.push_back(gas[i] - cost[i]);
+		}
+		if (a != 0)return -1;
+		if (a == 0) {
+			for (int i = 0; i < temp.size(); i++) {
+				if(temp)
+			}
+		}
+	}
+};
