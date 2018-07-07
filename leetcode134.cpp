a611bd77eb060ff35bc19ff8350a5b40b065c959
ParticularJ <344409038@qq.com>
2018-07-07 23:11:34 +0800

    leet

--- leetcode134.cpp
+++ leetcode134.cpp
@@ -4,24 +4,57 @@
 
 using namespace std;
 /*
-	
+	思路：一个环，首先把他拷贝一份，组成两个链表，这样可以随便从哪开始保证，全部走一遍
+	如 ： ABCDABCD
+		 + + - - ++ --
+	无论从哪一个开始均能走一个环
+	前缀和： 随便从哪开始，保证每一步运算结果都是正的即可
+	使用双端队列,单调队列，维护一个区间
 */
 
 class Solution {
 public:
 	int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
 		int size = gas.size();
-		int a = 0;
-		vector<int> temp;
 		for (int i = 0; i < size; i++) {
-			a += gas[i] - cost[i];
-			temp.push_back(gas[i] - cost[i]);
+			gas[i] -= cost[i];
 		}
-		if (a != 0)return -1;
-		if (a == 0) {
-			for (int i = 0; i < temp.size(); i++) {
-				if(temp)
+
+		// 转换成连起来的两条链
+		vector<int> lian(size * 2, 0);
+		lian[0] = gas[0];
+		for (int i = 1; i < size; i++) lian[i] = lian[i-1] + gas[i];
+		for (int i = size; i < 2 * size; i++)lian[i] = lian[i-1] + gas[i - size];
+		// 存储id和具体的值
+		deque<pair<int, int> > mini;
+		int pre_sum = 0;
+		for (int i = 0; i < size; i++) {
+			// 构建双端队列
+			if (i == 0) {
+				for (int i = 0; i < size; i++) {
+					if (mini.size() == 0) {
+						mini.push_back(make_pair(i, lian[i]));
+					}
+					else {
+						// 单调递增队列
+						while (mini.size() > 0 && mini.back().second > lian[i])
+							mini.pop_back();
+						mini.push_back(make_pair(i, lian[i]));
+					}
+				}
 			}
+			else {
+				// 维持一个范围
+				if (mini.front().first == i - 1)
+					mini.pop_front();
+				while (mini.size() > 0 && mini.back().second > lian[i + size - 1])
+					mini.pop_back();
+				mini.push_back(make_pair(i + size - 1, lian[i + size - 1]));
+			}
+			if (mini.front().second - pre_sum >= 0)
+				return i;
+			pre_sum = pre[i];
 		}
+		return -1;
 	}
 };
