fbcf30abecde7a5f8a8e8e8fd73c80af87886768
ParticularJ <344409038@qq.com>
2018-07-15 23:52:26 +0800

    leet

--- leetcode236.cpp
+++ leetcode236.cpp
@@ -4,7 +4,7 @@
 *     int val;
 *     TreeNode *left;
 *     TreeNode *right;
-*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {} 
 * };
 */
 
@@ -15,8 +15,14 @@
 class Solution {
 public:
 	TreeNode* res;
-	dfs(TreeNode* root, TreeNode* p, TreeNode* q) {
-		
+	pair<bool, bool> dfs(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (root == NULL)return make_pair(false, false);
+		auto l = dfs(root->left, p, q);
+		auto r = dfs(root->right, p, q);
+		r.first |= l.first|(l==p);
+		r.second |= l.second(r==q);
+		if (r.first&&r.second&&ret == NULL)ret = root;
+		return r;
 	}
 	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 		res = NULL;
