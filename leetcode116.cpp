9b2c47b99f9c81fe4acfb5867c31b3198d313fc2
ParticularJ <344409038@qq.com>
2018-07-21 23:54:30 +0800

    leet

---
+++ leetcode116.cpp
@@ -0,0 +1,26 @@
+/**
+* Definition for binary tree with next pointer.
+* struct TreeLinkNode {
+*  int val;
+*  TreeLinkNode *left, *right, *next;
+*  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+* };
+*/
+class Solution {
+public:
+	void dfs(TreeLinkNode* res, TreeLinkNode* temp) {
+		if(temp != NULL){
+			temp->next = res;
+		}
+		if (res->left != NULL) {
+			dfs(res->left, (temp == NULL)?NULL:temp->right);
+		}
+		if (res->right != NULL) {
+			dfs(res->right, res->left);
+		}
+	}
+	void connect(TreeLinkNode *root) {
+		if (root == NULL)return;
+		dfs(root, NULL);
+	}
+};
