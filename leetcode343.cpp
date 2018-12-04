8f9ebfc4f88cea7a40fcee545381c0a7bace6540
ParticularJ <344409038@qq.com>
2018-12-04 22:15:21 +0800

    leet

--- leetcode343.cpp
+++ leetcode343.cpp
@@ -16,16 +16,22 @@ Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
 class Solution {
 private:
 	int max3(int a, int b, int c) {
-		return max(max(a, b), c);
+		return max3(max(a, b), c);
 	}
 public:
 	int integerBreak(int n) {
 		assert(n >= 2);
+		// 设置记录值
 		vector<int> memo(n + 1, -1);
 		memo[1] = 1;
 		memo[2] = 1;
+		// 从3开始
 		for (int i = 3; i <= n; i++)
-			for (int j = 1; j < i; j++)
+			// 可以分割成1到i-1
+			for (int j = 1; j <= i-1; j++)
+				// 分别对比，取 当前的memo[i], 直接分割成j*(i-j),以及j*memo[i-j]的 最大值
+				// 比如i=3, j=1时， max(memo[3], memo[3-1]*1, 1*(3-1))的 memo[3] = 2
+				//  i=3, j=2时， max（memo[3], memo[3-2] * 1, 2*(3-2)得， memo[3] = 2
 				memo[i] = max3(memo[i], memo[i - j] * j, j*(i - j));
 		return memo[n];
 	}
