99101f2662524cecb076d94d5be62a30d704dcc7
ParticularJ <344409038@qq.com>
2018-12-12 14:43:25 +0800

    leet

--- leetcode213.cpp
+++ leetcode213.cpp
@@ -31,3 +31,61 @@ public:
 
 	}
 };
+
+
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+because they are adjacent houses.
+Example 2:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+*/
+class Solution {
+private:
+	vector<int> N;
+	//[start, end)
+	int robToN(int start, int end) {
+
+		vector<int> memo(end - start, -1);
+		memo[0] = N[start];
+		memo[1] = max(N[start + 1], memo[0]);
+		// 范围一定要算好
+		// N从start + i开始，因为有偏移量
+		for (int i = 2; i < end - start; i++) {
+			memo[i] = max(N[start + i] + memo[i - 2], memo[i - 1]);
+		}
+		return memo[end - start - 1];
+	}
+
+public:
+	int rob(vector<int>& nums) {
+		N = nums;
+		int size = nums.size();
+		if (size == 0)
+			return 0;
+		if (size >= 3) {
+			int rob1 = robToN(0, size - 1);
+			int rob2 = robToN(1, size);
+			return rob1 > rob2 ? rob1 : rob2;
+		}
+		else {
+			if (size == 1)
+				return nums[0];
+			else
+				return max(nums[0], nums[1]);
+		}
+
+	}
+};
