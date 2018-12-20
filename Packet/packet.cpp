aaf49d77238d24fa2e4c1e09d4705689e171eb76
ParticularJ <344409038@qq.com>
2018-12-20 21:53:18 +0800

    leet

--- Packet/packet.cpp
+++ Packet/packet.cpp
@@ -6,22 +6,34 @@ using namespace std;
 
 class Solution {
 private:
+	vector<vector<int>> memo;
 	int findValue(vector<int>& w, vector<int>& v, int index, int C) {
+		// 递归终止条件
+		// index 可以取到0，
 		if (index < 0 || C <= 0) {
 			return 0;
 		}
+		//判断是否计算过
+		if (memo[index][C] != -1)
+			return memo[index][C];
 
+
+		// 不管当前的结果
 		int res = findValue(w, v, index - 1, C);
-
+		// 判断是否能放下
 		if (C >= w[index]) {
+			// 不管当前和放下当前的结果大小
 			res = max(res, v[index] + findValue(w, v, index - 1, C - w[index]));
 		}
+		memo[index][C] = res;
+
 		return res;
 	}
 
 public:
 	int packet(vector<int>& w, vector<int>& v, int C) {
 		int size = w.size();
+		memo = vector<vector<int>>(size, vector<int>(C + 1, -1));
 		// 传入w, v, 遍历到的元素，以及容量
 		return findValue(w, v, size - 1, C);
 	}
