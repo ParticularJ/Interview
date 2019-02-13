d7476aeece1a2bc66057daeb6fc8cc4258dade30
ParticularJ <344409038@qq.com>
2019-02-13 23:44:26 +0800

    leet

--- leetcode49.cpp
+++ leetcode49.cpp
@@ -11,6 +11,7 @@ public:
   vector<vector<string>> groupAnagrams(vector<string>& strs){
     vector<vector<string>> res;
     unordered_map<string, int> mymap;
+
     if(strs.size()==0)return res;
     for(int i=0;i<strs.size();i++){
       string temp = strs[i];
